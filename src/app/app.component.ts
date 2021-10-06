f197707609b74ac5f7ff052187cbccbc6a3bfe95
hsiaofongw <hsiaofong.w@gmail.com>
2021-10-06 14:30:12 +0800

    chore: 更新了OAuth Client信息

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  clientId = 'Iv1.904dfc227b774cc7';
+  clientId = 'Iv1.ac6dbe5b3e366cf5';
   githubRequestAuthorizationBaseURL = 'https://github.com/login/oauth/authorize';
+  redirectUri = 'http://localhost:4200/github-oauth-redirect-back';
   loginRedirectTo?: SafeUrl;
 
   constructor(private router: Router, private domSanitizer: DomSanitizer) {}
@@ -17,6 +18,7 @@ export class AppComponent {
   ngOnInit(): void {
     const githubURLObject = new URL(this.githubRequestAuthorizationBaseURL);
     githubURLObject.searchParams.append('client_id', this.clientId);
+    githubURLObject.searchParams.append('redirect_uri', this.redirectUri);
     this.loginRedirectTo = this.domSanitizer.bypassSecurityTrustUrl(githubURLObject.toString())
   }
 }
